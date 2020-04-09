2e2e83fb99471c55fef836f4bb6bd8dcca32dd28
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-04-09 20:41:43 +0700

    Add files via upload

---
+++ LatihanInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TugasInheritance
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person orang = new Person("Budi", 28);
+            orang.GetNameAndAge();
+
+            Teacher guru = new Teacher("Daus", 34, "2881", "Math");
+            guru.GetNameAndAge();
+
+            Student siswa = new Student("Safira", 19, "2898", "safira@gmail.com");
+            siswa.GetNameAndAge();
+        }
+    }
+}
---
+++ LatihanInheritance/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TugasInheritance
+{
+    public class Teacher : Person
+    {
+        public Teacher(string nama, int umur, string idguru, string mapel)
+        : base(nama, umur)
+        {
+            TeacherId = idguru;
+            Subject = mapel;
+        }
+
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+
+    }
+}
