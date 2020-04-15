a1574db4bd359547d6411c56d3a87cf6871e6b7b
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-04-15 14:44:33 +0700

    Add files via upload

---
+++ ResponsiPemrograman2881/Karyawan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2881
+{
+    public class Karyawan
+    {
+        public Karyawan(int no, int nik, string nama, int gajibulanan)
+        {
+            No = no;
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+        }
+
+        public int No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public void gaji()
+        {
+            Console.WriteLine("{0}.  {1} {2}      {3}", No, Nik, Nama, GajiBulanan);
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("Maaf Gaji Kurang dari 0");
+            }
+        }
+
+        public void gajinaik()
+        {
+            Console.WriteLine("{0}.  {1} {2}     {3}", No, Nik, Nama, GajiBulanan + (GajiBulanan * 10 / 100));
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("Maaf Gaji Kurang dari 0");
+            }
+        }
+
+    }
+}
---
+++ ResponsiPemrograman2881/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ResponsiPemrograman2881
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Karyawan karyawan1= new Karyawan (1, 19112881, "Iqbal", 7000000);
+            karyawan1.gaji();
+            Karyawan karyawan2 = new Karyawan(2, 19112899, "Budi ", 5000000);
+            karyawan2.gaji();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("ASYIKKKKKKKK GAJI NAIK 10%!");
+            Console.WriteLine(" ");
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Karyawan karyawan3 = new Karyawan(1, 19112881, "Iqbal", 7000000);
+            karyawan3.gajinaik();
+            Karyawan karyawan4 = new Karyawan(2, 19112899, "Budi ", 5000000);
+            karyawan4.gajinaik();
+        }
+    }
+}
