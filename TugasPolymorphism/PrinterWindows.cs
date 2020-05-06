f22ccff64f57cad59205821f2529beb231e11ab3
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-05-06 15:13:15 +0700

    Add files via upload

---
+++ TugasPolymorphism/PrinterWindows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasPolymorphism
+{
+    public class PrinterWindows
+    {
+        public string Name { get; set; }
+
+        public virtual void Show()
+        {
+        }
+
+        public virtual void Print()
+        { 
+        }
+    }
+}
