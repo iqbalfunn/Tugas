6ba0a5c810b2c90d9c58a6f049011fd87c762ea3
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-06-11 18:25:22 +0700

    Add files via upload

---
+++ Abstraction/ISuara.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public interface ISuara
+    {
+        void View();
+    }
+
+    class Suara : ISuara
+    {
+        public void View()
+        {
+            Console.WriteLine("Suara-Suara Kendaraan");
+            Console.WriteLine("Berikut Contohnya : ");
+            Console.WriteLine();
+        }
+    }
+}
