5cba41fa80c7cd43f2c0c4652f254124a68e8e59
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-06-17 21:30:42 +0700

    Add files via upload

---
+++ TugasCollection/KaryawanHarian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasLab8
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public override string Nik { get; set; }
+        public override string Nama { get; set; }
+        public double UpahPerJam { get; set; }
+        public double JumlahJamKerja { get; set; }
+        public override double Gaji() { return UpahPerJam * JumlahJamKerja; }
+    }
+}
---
+++ TugasCollection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TugasLab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tugas Lab 8 (Pertemuam 11)";
+
+            // membuat objek untuk semua karyawan
+
+            KaryawanTetap karyawanTetap = new KaryawanTetap();
+            karyawanTetap.Nik = "4324132";
+            karyawanTetap.Nama = "Iqbal Fanani";
+            karyawanTetap.GajiBulanan = 8000000;
+
+            KaryawanHarian karyawanHarian = new KaryawanHarian();
+            karyawanHarian.Nik = "3423243";
+            karyawanHarian.Nama = "Budi Hartono";
+            karyawanHarian.JumlahJamKerja = 8;
+            karyawanHarian.UpahPerJam = 15000;
+
+            Sales sales = new Sales();
+            sales.Nik = "6464564";
+            sales.Nama = "Jimmy Sutanto";
+            sales.JumlahPenjualan = 30;
+            sales.Komisi = 50000;
+
+            // object class collection 
+            List<Karyawan> listKaryawan = new List<Karyawan>();
+
+            listKaryawan.Add(karyawanTetap);
+            listKaryawan.Add(karyawanHarian);
+            listKaryawan.Add(sales);
+
+            int noUrut = 1;
+
+            foreach (Karyawan karyawan in listKaryawan)
+            {
+                Console.WriteLine("{0}. Nik : {1}, Nama : {2}, Gaji : Rp.{3 : 0}",
+                    noUrut, karyawan.Nik, karyawan.Nama, karyawan.Gaji());
+
+                noUrut++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
