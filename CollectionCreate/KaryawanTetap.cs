096789f29aa5bad5da35123e12f61107b1a37978
iqbalfunn <57314694+iqbalfunn@users.noreply.github.com>
2020-06-29 00:32:26 +0700

    Add files via upload

---
+++ CollectionCreate/KaryawanTetap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionCreate
+{
+    public class KaryawanTetap : Karyawan
+    {
+        public override string Nik { get; set; }
+        public override string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+        public override double Gaji() { return GajiBulanan; }
+    }
+}
---
+++ CollectionCreate/Sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionCreate
+{
+    public class Sales : Karyawan
+    {
+        public override string Nik { get; set; }
+        public override string Nama { get; set; }
+        public double JumlahPenjualan { get; set; }
+        public double Komisi { get; set; }
+        public override double Gaji() { return JumlahPenjualan * Komisi; }
+    }
+}
